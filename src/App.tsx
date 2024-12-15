30eb35ed8cfd2726ee2021424899f9e6dc418001
swaminaing <smnaing9.smn@gmail.com>
2024-12-15 13:07:24 +0630

    working with type predicates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Input from './components/Input'
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   return (
-    <>
-      <Input id='name' label='Your name' type="text" />
-      <Input id='age' label='Your age' type="text" />
-    </>
-  )
+    <main>
+      {/* <Input id='name' label='Your name' type="text" />
+      <Input id='age' label='Your age' type="text" /> */}
+
+      <p>
+        <Button>A Button</Button>
+      </p>
+
+      <p>
+        <Button href="https://google.com">
+          A Link
+        </Button>
+      </p>
+    </main>
+  );
 }
 
-export default App
+export default App;
