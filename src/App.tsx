c971c93dfe317f5f3f7237671284f0472d4a4ebf
swaminaing <smnaing9.smn@gmail.com>
2024-12-20 21:29:23 +0630

    create TimersContextProvider function and passes it to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import AddTimer from './components/AddTimer.tsx';
 import Header from './components/Header.tsx';
+import TimersContextProvider from './components/store/timers-context.tsx';
 import Timers from './components/Timers.tsx';
 
 function App() {
   return (
-    <>
+    <TimersContextProvider>
       <Header />
       <main>
         <AddTimer />
         <Timers />
       </main>
-    </>
+    </TimersContextProvider>
   );
 }
 
--- src/components/store/timers-context.tsx
+++ src/components/store/timers-context.tsx
@@ -1,19 +1,41 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 
 type Timer = {
-    name: string;
-    duration: number;
-}
+  name: string;
+  duration: number;
+};
 
 type TimerState = {
-    isRunning: boolean;
-    timers: Timer[];
-}
+  isRunning: boolean;
+  timers: Timer[];
+};
 
 type TimersContextValue = TimerState & {
-    addTimer: (timeData: Timer) => void;
-    startTimer: () => void;
-    stopTimer: () => void;
-}
+  addTimer: (timeData: Timer) => void;
+  startTimer: () => void;
+  stopTimer: () => void;
+};
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({ children }: TimersContextProviderProps) {
+    const ctx: TimersContextValue = {
+        timers: [],
+        isRunning: false,
+        addTimer(timeData){
+            //...
+        },
+        startTimer(){
+
+        }, 
+
+        stopTimer(){
+
+        }
+    }
+  return <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>;
+}
